689133afa9a9b18f1038bc50a424bf5c1c87d807
Masatomi KINO <masatomix@ki-no.org>
2019-01-19 08:54:30 +0900

    Bootstrap-vue をつかってすこしだけデザイン
    npm install bootstrap-vue --save したのち、
    main.jsに
    
    import BootstrapVue from "bootstrap-vue"
    import "bootstrap/dist/css/bootstrap.min.css"
    import "bootstrap-vue/dist/bootstrap-vue.css"
    Vue.use(BootstrapVue)
    
    を追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,21 @@
 <template>
-  <header>
-    <span @click="auth.login()" v-if='!loginStatus' class='button' >login</span>
-    <span @click="auth.logout()" v-if='loginStatus' class='button' >{{user.displayName}}さんを logout</span>
-  </header>
+  <b-navbar toggleable="md" type="dark" variant="info">
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+  <b-navbar-brand href="#">ToDo管理</b-navbar-brand>
+  <b-collapse is-nav id="nav_collapse">
+    <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item-dropdown right>
+        <!-- Using button-content slot -->
+        <template slot="button-content">
+          <em><span v-if='!loginStatus' >未ログイン</span><span v-if='loginStatus' >{{user.displayName}}</span></em>
+        </template>
+        <b-dropdown-item @click="auth.login()" v-if='!loginStatus' >Signin</b-dropdown-item>
+        <b-dropdown-item @click="auth.logout()" v-if='loginStatus' >{{user.displayName}}さんを Signout</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+  </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
@@ -29,9 +42,4 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-.button{
-  cursor: pointer;
-  font-size: 12px;
-  color:steelblue
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+
 import router from './router'
 import store from '@/store'
 import firebase from 'firebase'
 import firebaseConfig from '@/firebaseConfig'
+
+import BootstrapVue from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+Vue.use(BootstrapVue)
+
 
 // if (!firebase.apps.length) {
 firebase.initializeApp(firebaseConfig)
