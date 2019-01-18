c34e7956be8578e5bd1797222b9d472c59c1afe1
Masatomi KINO <masatomix@ki-no.org>
2019-01-18 16:04:59 +0900

    追加した認証機能を使って、
    認証されているときだけ、画面を表示するように機能追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="hello">
+  <main v-if='$store.state.loginStatus'>
     <h1>
-      <button @click="deleteEndTask">完了タスクの削除</button>My Todo Task<span class='info'>({{Object.keys(remainingTask).length}}/{{Object.keys(todos).length}})</span></h1>
+      <button @click="deleteEndTask">完了タスクの削除</button>My Todo Task<span class='info'>({{remainingTask.length}}/{{todos.length}})</span></h1>
     <ul>
-      <li v-for='(todo,key) in todos' :key='key'>
-        <input type='checkbox' v-model='todo.isDone'>
+      <li v-for='todo in todos' :key='todo.id'>
+        <input type='checkbox' v-model='todo.isDone' @click='done(todo)' >
         <span v-bind:class='{done: todo.isDone}'>{{todo.name}}</span>
-        <span @click='deleteTask(key)' class='xButton'>[x]</span>
+        <span @click='deleteTask(todo.id)' class='xButton'>[x]</span>
         </li>
     </ul>
     <form @submit.prevent='addTask'>
       <input type='text' v-model='newTask'>
       <input type='submit' value='追加' >
     </form>
-  </div>
+  </main>
 </template>
 
 <script>
@@ -24,17 +24,9 @@ export default {
   data () {    // 画面で使用する変数を定義する場所
     return {
       newTask: '',
-      todos: {},
+      todos: [],
       db: firebase.firestore()
     }
-  },
-  watch: {
-    // todos: {
-    //   handler: function() {
-    //     // localStorage.setItem("todos", JSON.stringify(this.todos));
-    //   },
-    //   deep: true
-    // }
   },
   created: function () {
     // this.todos = JSON.parse(localStorage.getItem("todos")) || [];
@@ -44,9 +36,8 @@ export default {
       this.loading = false
       querySnapshot.forEach(doc => {
         const task = doc.data()
-        // me.todos[doc.id]=task
-        me.$set(me.todos, doc.id, task);
-        // https://jp.vuejs.org/2016/02/06/common-gotchas/
+        task.id = doc.id
+        me.todos.push(task)
       })
     })
   },
@@ -54,6 +45,7 @@ export default {
     addTask: function () {
       const me = this
       const task = {
+        id: '',
         name: this.newTask,
         isDone: false
       }
@@ -61,57 +53,48 @@ export default {
         .collection('todos')
         .add(task)
         .then(function (docref) {
-          // me.todos[docref.id]=task
-          me.$set(me.todos, docref.id, task);
+          task.id = docref.id
+          me.todos.push(task)
         })
       this.newTask = ''
     },
-
+    done: function(todo){ 
+      this.db
+        .collection("todos")
+        .doc(todo.id)
+        .set(todo)
+    },
     deleteTask: function (key) {
-      this.$delete(this.todos, key)
+      this.todos.forEach((todo, index) => {
+        if (todo.id == key) {
+          this.todos.splice(index, 1)
+        }
+      })
       this.db
         .collection('todos')
         .doc(key)
         .delete()
     },
     deleteEndTask: function () {
-      const doneTasks = {}
-      Object.keys(this.todos)
-        .filter(key => this.todos[key].isDone)
-        .forEach(key => {
-          doneTasks[key] = this.todos[key]
-          this.db
-            .collection("todos")
-            .doc(key)
-            .delete()
-        })
+      const doneTasks = this.todos.filter(todo => {
+        return todo.isDone
+      })
+      
       this.todos = this.remainingTask
+
+      for (let task of doneTasks) {
+        this.db
+          .collection('todos')
+          .doc(task.id)
+          .delete()
+      }
     }
-
-
   },
   computed: {
     remainingTask: function () {
-
-      // 関数をつかう場合
-      const me = this
-      const remainingTask = {}
-      Object.keys(this.todos)
-        .filter(function(key) {
-          return !me.todos[key].isDone
-        })
-        .forEach(key => (remainingTask[key] = me.todos[key]));
-        
-    
-      // // Arrow関数で 簡単に書いた場合
-      // return Object.keys(this.todos)
-      //   .filter(key => !this.todos[key].isDone)
-      //   .map(key => this.todos[key])
-
-      // return this.todos.filter(function (todo) {
-      //   return !todo.isDone
-      // })
-      return remainingTask   
+      return this.todos.filter(function (todo) {
+        return !todo.isDone
+      })
     }
   }
 }
